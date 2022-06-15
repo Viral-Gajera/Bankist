d0dd446b8099e90fde612ddb4d7ddcc6e002c855
Viral-Gajera <viral.gajera218@gmail.com>
2022-06-15 13:17:53 +0530

    cookie message created

--- script.js
+++ script.js
@@ -5,14 +5,29 @@ function initialSetting(){
     // highlighting "open account"
     links[ links.length - 1 ].classList.add('active');
 }
-
 initialSetting();
 
-for( let i=0; i<links.length; i++ ){
-    links[i].addEventListener('click', function(){
-        for(let j=0; j<links.length; j++){
-            links[j].classList.remove('active')
+
+// navigation-bar 'activation' and 'deactivation'
+for(let link of links ){
+    link.addEventListener('click', function(){
+        for(let l of links ){
+            l.classList.remove('active');
         }
-        this.classList.add('active')
+        this.classList.add('active');
     })
 }
+
+
+// Showing Cookie message
+let cookieMessage = document.createElement('div');
+
+cookieMessage.classList.add('cookieMessage');
+cookieMessage.innerHTML = `Accept cookie to improve performence <button class="cookie-button">Got it</button>`;
+
+document.body.appendChild(cookieMessage);
+
+document.querySelector('.cookie-button').addEventListener('click', function(){
+    cookieMessage.remove();
+})
+
